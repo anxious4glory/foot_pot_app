edebe55c285b9aa65ec849c330dd552619ec5f52
anxious4glory <53601508+anxious4glory@users.noreply.github.com>
2021-11-17 03:30:31 +0100

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
+const express = require('express');
+const path = require('path');
 
-burger.addEventListener('click', ()=> {
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
+const app = express();
+const port = "3000";
+
+// set template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html'); 
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.sendFile((__dirname, 'index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log('listening On Port 3000');
 });
